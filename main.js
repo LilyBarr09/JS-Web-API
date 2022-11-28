738f5b6740b1ff7100563d23472a887d4ea8b3ba
Liliana Barrera <barrera.jimnlil@gmail.com>
2022-11-28 12:15:56 -0500

    added html

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-const promise = new Promise ((resolve, reject) => {
-	resolve('Heck Yeah!')
-});
+const cookies = fetch('https://freerandomapi.cyclic.app/api/v1/desserts?category=Cookie&limit=10');
+const donuts = fetch('https://freerandomapi.cyclic.app/api/v1/desserts?category=Donut&limit=10');
+const iceCream = fetch('https://freerandomapi.cyclic.app/api/v1/desserts?category=Ice_Cream&limit=10');
 
-promise
-.then((data) => console.log(data));
+Promise
+.all([cookies, donuts, iceCream])
+.then((response) => 
+Promise.all(response.map((res) => res.json()))
+.then((data) => console.log(data))); //place data in a desserts variable
+
+
+
